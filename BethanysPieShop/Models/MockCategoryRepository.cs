8d84798fced28e2798a1590bde9835913c92fe75
tiago-laranjeiro <tiago-laranjeiro@users.noreply.github.com>
2021-02-04 12:33:15 +0100

    Add project files.

---
+++ BethanysPieShop/Models/MockCategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public class MockCategoryRepository : ICategoryRepository
+    {
+        public IEnumerable<Category> AllCategories =>
+            new List<Category>
+            {
+                new Category{CategoryId=1, CategoryName="Fruit pies", Description="All-fruit pie"},
+                new Category{CategoryId=2, CategoryName="Cheese cakes", Description="Cheesy pie"},
+                new Category{CategoryId=3, CategoryName="Seasonal pies", Description="Nowadays pie"}
+            };
+    }
+}
